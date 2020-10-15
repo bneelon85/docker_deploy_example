519535f4f70bdffb0d76a379019c45ae0d496986
Bobby Neelon <robert.neelon@gmail.com>
2020-10-15 12:56:42 -0500

    First Commit

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+# initialize our Flask application
+app= Flask(__name__)
+@app.route("/post-to-me", methods=["POST"])
+def postImageToSlack():
+    if request.method=='POST':
+        posted_data = str(request.get_json())
+        return jsonify("You sent {}".format(posted_data))
+
+#  main thread of execution to start the server
+if __name__=='__main__':
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=80)
